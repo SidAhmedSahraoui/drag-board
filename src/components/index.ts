f080fb7ab16ae19088592f175f8c383826847ff5
SidAhmedSahraoui <s.sahraoui@esi-sba.dz>
2024-09-15 22:40:56 +0100

    Refactor code to remove unused imports and files

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-import { lazy } from "react";
-
-export * from "./item/item";
-export * from "./input/input";
-export * from "./button/button";
-export * from "./container/container";
-
-export const Modal = lazy(() => import("./modal/modal"));
